f98264f11f092b006bad4accff323c220243b47b
yvictorck <yvictorck@gmail.com>
2021-12-08 15:26:48 +0800

    Update index.js

--- audio/index.js
+++ audio/index.js
@@ -13,6 +13,13 @@ app.get('/audio', (req, res) => {
   res.sendFile('./audio.html', { root: __dirname });
 });
 
+
+app.get('/*', (req, res) => {
+  console.log(req.originalUrl);
+  res.sendFile(req.originalUrl+'.html', { root: __dirname });
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
